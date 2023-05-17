b943e37b81b4407adfaadaf6257712ca9a62c3c8
suvro47 <sohel.cse.hstu@gmail.com>
2023-05-17 16:01:53 +0600

    chunk size read form env

--- src/main/java/com/dsi/ieims/csv/processor/App.java
+++ src/main/java/com/dsi/ieims/csv/processor/App.java
@@ -1,13 +1,11 @@
 package com.dsi.ieims.csv.processor;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
 public class App implements CommandLineRunner {
 
     @Autowired
@@ -21,6 +19,6 @@ public class App implements CommandLineRunner {
     }
 
     @Override public void run(String... args) throws Exception {
-        csvFileProcessor.process(source, destination, 5);
+        csvFileProcessor.process(source, destination, 1000);
     }
 }
